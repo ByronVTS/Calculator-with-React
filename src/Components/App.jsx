694c23b550a6f0ccb3ba556b1034697982494d69
Drend <byronthyto@gmail.com>
2024-02-01 21:37:21 -0500

    Añadiendo archivos para el proyecto

---
+++ src/Components/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+  
+
+  return (
+
+    <>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand " href="#">
+            Calculadora
+          </a>
+        </div>
+      </nav>      
+    </>
+  )
+}
+
+export default App
